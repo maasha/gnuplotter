e67dccde9de67ddd8e13281040149d792c3df8ac
Martin A. Hansen <mail@maasha.dk>
2015-01-21 16:53:58 +0100

    added code and test files

---
+++ lib/gnuplotter.rb
@@ -0,0 +1,2 @@
+class GnuPlotter
+end
---
+++ test/gnuplotter/test_gnuplotter.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+$:.unshift File.join(File.dirname(__FILE__), '..', '..')
+
+require 'test/helper'
+
+class TestGnuPlotter < Test::Unit::TestCase 
+  def setup
+  end
+
+  test "GnuPliotter::.new" do
+  end
+end
