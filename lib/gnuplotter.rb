730e8cbc24e1e9844b11f22d0e428c68a80cc66b
Martin A. Hansen <mail@maasha.dk>
2015-01-23 13:37:58 +0100

    build complete gnuplot scripts OK

--- lib/gnuplotter.rb
+++ lib/gnuplotter.rb
@@ -1,4 +1,7 @@
 require 'gnuplotter/version'
+require 'tempfile'
+
+class GnuPlotterError < StandardError; end
 
 class GnuPlotter
   NOQUOTE = [
@@ -25,7 +28,6 @@ class GnuPlotter
     :ztics
   ]
 
-
   # Constructor method for a GnuPlot object.
   def initialize
     @options  = Hash.new { |h1, k1| h1[k1] = Hash.new { |h2, k2| h2[k2] = [] } }
@@ -35,7 +37,7 @@ class GnuPlotter
   # Method to set an option in the GnuPlot environment e.g:
   # set(title: "Nobel Prize")
   def set(options)
-    raise unless options.is_a? Hash
+    raise GnuPlotterError, "Non-hash options given" unless options.is_a? Hash
 
     options.each do |key, value|
       @options[:set][key.to_sym] << value
@@ -47,7 +49,7 @@ class GnuPlotter
   # Method to unset an option in the GnuPlot environment e.g:
   # unset(ytics: true)
   def unset(options)
-    raise unless options.is_a? Hash
+    raise GnuPlotterError, "Non-hash options given" unless options.is_a? Hash
 
     options.each do |key, value|
       @options[:unset][key.to_sym] << value || true
@@ -56,23 +58,124 @@ class GnuPlotter
     self
   end
 
-  def to_s
-    lines = []
+  # Method that returns lines of gnuplot commands, options and data.
+  def to_s(cmd = "plot")
+    cmd_lines = []
 
     @options.each do |method, options|
       options.each do |key, list|
         list.each do |value|
           if value == :true or value === true
-            lines << %Q{#{method} #{key}}
+            cmd_lines << %Q{#{method} #{key}}
           elsif NOQUOTE.include? key.to_sym
-            lines << %Q{#{method} #{key} #{value}}
+            cmd_lines << %Q{#{method} #{key} #{value}}
           else
-            lines << %Q{#{method} #{key} "#{value}"}
+            cmd_lines << %Q{#{method} #{key} "#{value}"}
           end
         end
       end
     end
 
-    lines.join $/
+    cmd_lines  = cmd_lines.join($/)
+    opt_lines  = @datasets.inject([]) { |list, dataset| list << dataset.format_options }.join(", ")
+    data_lines = @datasets.inject([]) { |list, dataset| list << dataset.format_data.join($/) }.join("#$/e#$/")
+
+    if opt_lines.empty?
+      lines = [cmd_lines].join $/
+    elsif data_lines.empty?
+      lines = [cmd_lines, "#{cmd} " + opt_lines].join $/
+    else
+      lines = [cmd_lines, "#{cmd} " + opt_lines, data_lines].join $/
+    end
+
+    lines + $/
+  end
+
+  # Method to add a dataset to the current GnuPlot.
+  #   add_dataset(using: "1:2:3:4", with: "vectors nohead", title: "'bar'") do |plotter|
+  #     data2.map { |d| plotter << d }
+  #   end
+  def add_dataset(options = {})
+    raise GnuPlotterError, "No block given" unless block_given?
+
+    dataset = DataSet.new(options)
+    @datasets << dataset
+
+    yield dataset
+  end
+
+  # Method to execute the plotting of added datasets.
+  def plot
+    #@datasets.each { |dataset| dataset.close }
+
+    result = nil
+
+    Open3.popen3("gnuplot -persist") do |stdin, stdout, stderr, wait_thr|
+
+      if @datasets.empty?
+        lines << "plot 1/0"
+      else
+        lines << "plot " + @datasets.map { |dataset| dataset.to_gp }.join(", ")
+      end
+
+      lines.map { |l| $stderr.puts l } if $VERBOSE
+      lines.map { |l| stdin.puts l }
+
+      stdin.close
+      result = stdout.read
+      stdout.close
+
+      exit_status = wait_thr.value
+
+      unless exit_status.success?
+        raise GnuPlotterError, stderr.read
+      end
+    end
+
+    result
+  end
+
+  def splot
+  end
+
+  # Nested class for GnuPlot datasets.
+  class DataSet
+    def initialize(options = {})
+      @options = options
+      @data    = []
+    end
+
+    # Write method.
+    def <<(*obj)
+      @data << obj
+    end
+
+    alias :write :<<
+
+    # Method that builds a plot/splot command string from dataset options.
+    def format_options
+      options = []
+      options << %Q{"-"}
+
+      @options.each do |key, value|
+        if value == :true
+          options << "#{key}"
+        else
+          options << "#{key} #{value}"
+        end
+      end
+
+      options.join(" ")
+    end
+
+    def format_data
+      lines = []
+
+      @data.each do |row|
+        lines << row.join(", ")
+      end
+
+      lines
+    end
   end
 end
--- test/test_gnuplotter.rb
+++ test/test_gnuplotter.rb
@@ -5,15 +5,93 @@ require 'test/helper'
 
 class TestGnuPlotter < Test::Unit::TestCase 
   def setup
+    @data1 = [
+      [0, 0, 0.5,  0.5],
+      [0, 1, -0.5, 0.5],
+      [1, 1, 1,    0]
+    ]
+
+    @data2 = [
+      [10, 10, 1.5,  1.5],
+      [10, 11, -1.5, 1.5],
+      [11, 11, 11,   10]
+    ]
+
+    @gp = GnuPlotter.new.set(title: "test").set(terminal: "dumb")
+  end
+
+  test "#set with non-hash options raises" do
+    assert_raise(GnuPlotterError) { GnuPlotter.new.set("title") }
+  end
+
+  test "#unset with non-hash options raises" do
+    assert_raise(GnuPlotterError) { GnuPlotter.new.set("title") }
+  end
+
+  test "#to_s with nothing set and no data returns empty" do
   end
 
   test "#to_s with set quoted and unquoted options returns correctly" do
     gp = GnuPlotter.new.set(title: "test").set(terminal: "dumb")
-    assert_equal(%Q{set title "test"\nset terminal dumb}, gp.to_s)
+    assert_equal(%Q{set title "test"\nset terminal dumb\n}, gp.to_s)
   end
 
   test "#to_s with unset quoted and unquoted options returns correctly" do
     gp = GnuPlotter.new.unset(title: true).unset(terminal: true)
-    assert_equal(%Q{unset title\nunset terminal}, gp.to_s)
+    assert_equal(%Q{unset title\nunset terminal\n}, gp.to_s)
   end
+
+  test "#add_dataset without block raises" do
+    assert_raise(GnuPlotterError) { @gp.add_dataset }
+  end
+
+  test "#to_s with one dataset returns correctly" do
+    @gp.add_dataset(using: "1:2:3:4", with: "vectors nohead", title: "'foo'") do |plotter|
+        @data1.map { |d| plotter << d }
+    end
+
+    expected = <<END
+set title "test"
+set terminal dumb
+plot "-" using 1:2:3:4 with vectors nohead title 'foo'
+0, 0, 0.5, 0.5
+0, 1, -0.5, 0.5
+1, 1, 1, 0
+END
+
+    assert_equal(expected, @gp.to_s)
+  end
+
+  test "#to_s with two datasets returns correctly" do
+    @gp.add_dataset(using: "1:2:3:4", with: "vectors nohead", title: "'foo'") do |plotter|
+        @data1.map { |d| plotter << d }
+    end
+
+    @gp.add_dataset(using: "1:2:3:4", with: "vectors nohead", title: "'bar'") do |plotter|
+        @data2.map { |d| plotter << d }
+    end
+
+    expected = <<END
+set title "test"
+set terminal dumb
+plot "-" using 1:2:3:4 with vectors nohead title 'foo', "-" using 1:2:3:4 with vectors nohead title 'bar'
+0, 0, 0.5, 0.5
+0, 1, -0.5, 0.5
+1, 1, 1, 0
+e
+10, 10, 1.5, 1.5
+10, 11, -1.5, 1.5
+11, 11, 11, 10
+END
+
+    assert_equal(expected, @gp.to_s)
+  end
+
+#  test "#plot produces something" do
+#    assert_equal("", @gp.plot)
+#  end
+
+#  test "#splot produces something" do
+#    assert_equal("", @gp.splot)
+#  end
 end
